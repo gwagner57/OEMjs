a2cdeeb460326bb2183f5a167e7377b9b66aff70
Benji Miethke <benji.miethke@b-tu.de>
2023-04-23 22:16:31 +0200

    Add RadioButtonChoiceWidget

--- src/ui/RadioButtonChoiceWidget.mjs
+++ src/ui/RadioButtonChoiceWidget.mjs
@@ -1,62 +1,61 @@
-import { dt, rECORD } from "../../datatypes.mjs";
-import { MandatoryValueConstraintViolation } from "../../constraint-violation-error-types.mjs";
+import { dt, rECORD, lIST } from "../datatypes.mjs";
+import bUSINESSoBJECT from "../bUSINESSoBJECT.mjs";
+import { MandatoryValueConstraintViolation } from "../constraint-violation-error-types.mjs";
 
 
 class RadioButtonChoiceWidget extends HTMLFieldSetElement {
-    constructor(recordsOrClass, { legend, disabled, validate } = {}) {
+    constructor(record, { legend, disabled } = {}) {
         super();
         this.htmlLegend = legend;
         this.disabled = disabled;
-        this.validate = validate;
-        this.props = null
-        // Use as Class
-        if (dt.isClass(recordsOrClass)) {
-            // Handle a lIST or Array property
-            if (!("instances" in recordsOrClass) || Object.keys(recordsOrClass.instances).length === 0) {
-                throw new Error(`No instances defined for class ${recordsOrClass.name}`);
-            }
-            if ("properties" in recordsOrClass) {
-                this.props = recordsOrClass.properties;
-            }
-            if ("displayAttributes" in EntityType) {
-                this.displayAttributes = recordsOrClass.displayAttributes;
-                if ("disabled" in recordsOrClass) {
-                    if (!recordsOrClass.disabled.every(a => recordsOrClass.displayAttributes.includes(a))) {
-                        throw new Error(`Disabled attributes do not include in desplayAttributes for ${recordsOrClass.name}`);
-                    }
-                }
-            }
+        // Handle a lIST or Array property
+        if (!("instances" in record) || Object.keys(record.instances).length === 0) {
+            throw new Error(`No instances defined for class ${record.name}`);
         }
-        entityIDs = Object.keys(recordsOrClass);
+        this.record = record;
     }
 
     createFieldsetAttributes() {
-        let attr = "";
         if (this.disabled) {
-            attr += "disabled";
+            this.setAttribute("disabled", true);
         }
-        return attr;
     }
 
-    createInputAndLabel(singleRecord) {
-        // todo generate unique name for input field
-        if (singleRecord instanceof rECORD) {
-            if (Array.isArray(singleRecord.fieldNames)) {
-                let name = singleRecord.fieldNames;
-                let nameId = name + name.length;
-                let inputValue = singleRecord.fieldTypes;
-                let checkbox = `<input type="radio" id="${nameId}" name="${name}" value="${inputValue}" />
-            <label for="${nameId}">${name}</label>`
-            }
-        } else {
-            throw new MandatoryValueConstraintViolation(`Provided record ${singleRecord} isn't type rECORD`)
-        }
+    createInputAndLabel() {
+        // creates input and label nodes for each element
+        let innerHTML = "";
+        Object.keys(this.record.instances).forEach(oid => {
+            const elem = this.record.instances[oid];
+            let idKey = elem.idAttribute || oid;
+            let nameId = idKey + "_" + elem.name;
+            innerHTML += `<input type="radio" id="${nameId}" value="${elem.name}" />
+                    <label for="${nameId}">${elem.name}</label>`;
+        });
+        return innerHTML;
     }
 
     connectedCallback() {
+        this.createFieldsetAttributes();
+        this.setAttribute("data-bind", this.htmlLegend);
         let legend_element = `<legend>${this.htmlLegend}</legend>`
-        this.innerHTML = `<fieldset ${this.createFieldsetAttributes()}>${this.legend_element}${this.createInputAndLabel}</fieldset>`
+        this.innerHTML = `${legend_element}${this.createInputAndLabel()}`
+        this.addEventListener("change", e => console.log("Change event", e));
+        this.addEventListener("click", e => {
+            if (e.originalTarget.nodeName === "INPUT") {
+                let checked = e.originalTarget.checked;
+                if (checked === true) {
+                    // TODO checked is always true
+                    // e.originalTarget.checked = false;
+                }
+            }
+        });
+    }
+    disconnectedCallback() {
+        this.removeEventListener("change");
+        this.removeEventListener("click");
     }
 }
 
 customElements.define("radio-button-choice", RadioButtonChoiceWidget, { extends: "fieldset" });
+
+export default RadioButtonChoiceWidget;
